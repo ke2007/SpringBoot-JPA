70f346968113889e82eb2a2b2d9af97149704a44
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-09-08 18:37:57 +0900

    2022-09-08 Transactional을 통한 변경감지 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+
+    @Transactional //트랜잭셔널을 통한 변경감지
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+
     }
 
     public List<Item> findItems() {
